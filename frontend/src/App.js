ff0a30ff97d8074b31d06e0a64d2a7038ad1e58d
Kevin Wang <kcwang06@gmail.com>
2019-12-08 11:40:50 -0500

    kevin edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,27 @@ import Chats from "./pages/Chats";
 function App() {
   return (
     <div className="App">
+      <div className ="main">
+      <div className="main-title">
+        Polar
+      </div>
       <Router>
         <div className="navlinks">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="navbar-nav mr-auto">
+          <nav className="navbars">
+              <div className="navbar-nav">
                 <Link to={'/'} className="nav-link"> Home </Link>
-                <Link to={'/chats'} className="nav-link">Chats</Link>
-                <Link to={'/profile'} className="nav-link">Profile </Link>
+                {/* <Link to={'/chats'} className="nav-link">Chats</Link> */}
+                <Link to={'/profilse'} className="nav-link">Profile </Link>
               </div>
           </nav>
             <Switch>
                 <Route exact path='/' component={Home} />
-                <Route path='/chats' component={Chats} />
+                {/* <Route path='/chats' component={Chats} /> */}
                 <Route path='/profile' component={Profile} />
             </Switch>
           </div>
-          </Router>
+      </Router>
+      </div>
     </div>
   );
 }
