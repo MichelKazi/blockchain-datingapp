e8f24842bd50e82a04c249b929e2d324567f65b4
Michelkazi <michel.m.kazi@gmail.com>
2019-12-08 12:05:44 -0500

    implemented the eosjs boilerplate and changed up accounts in index.jsx to work with my smart contract

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,211 @@
+import React, { Component } from 'react';
+import { Api, JsonRpc, RpcError } from 'eosjs'; // https://github.com/EOSIO/eosjs
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig'
+import { TextDecoder, TextEncoder } from 'text-encoding';
+
+// material-ui dependencies
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+// eosio endpoint
+const endpoint = "http://localhost:8888";
+
+// NEVER store private keys in any source code in your real life development
+// This is for demo purposes only!
+const accounts = [
+  {"name":"default", "privateKey":"5KYEwpEdtKdBXWdjGEDYfc4hfQe9MKrW1Nq9pVwffnuRYeXuU6J", "publicKey":"EOS7RUHcEm9HHwDYo14CueHYeoa19VFDtKjEs9BiD6iL86PocFbnP"}
+  
+];
+// set up styling classes using material-ui "withStyles"
+const styles = theme => ({
+  card: {
+    margin: 20,
+  },
+  paper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  formButton: {
+    marginTop: theme.spacing.unit,
+    width: "100%",
+  },
+  pre: {
+    background: "#ccc",
+    padding: 10,
+    marginBottom: 0,
+  },
+});
+
+// Index component
+class Index extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      noteTable: [] // to store the table rows from smart contract
+    };
+    this.handleFormEvent = this.handleFormEvent.bind(this);
+  }
+
+  // generic function to handle form events (e.g. "submit" / "reset")
+  // push transactions to the blockchain by using eosjs
+  async handleFormEvent(event) {
+    // stop default behaviour
+    event.preventDefault();
+
+    // collect form data
+    let account = event.target.account.value;
+    let privateKey = event.target.privateKey.value;
+    let note = event.target.note.value;
+
+    // prepare variables for the switch below to send transactions
+    let actionName = "";
+    let actionData = {};
+
+    // define actionName and action according to event type
+    switch (event.type) {
+      case "submit":
+        actionName = "update";
+        actionData = {
+          user: account,
+          note: note,
+        };
+        break;
+      default:
+        return;
+    }
+
+    // eosjs function call: connect to the blockchain
+    const rpc = new JsonRpc(endpoint);
+    const signatureProvider = new JsSignatureProvider([privateKey]);
+    const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+    try {
+      const result = await api.transact({
+        actions: [{
+          account: "notechain",
+          name: actionName,
+          authorization: [{
+            actor: account,
+            permission: 'active',
+          }],
+          data: actionData,
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      });
+
+      console.log(result);
+      this.getTable();
+    } catch (e) {
+      console.log('Caught exception: ' + e);
+      if (e instanceof RpcError) {
+        console.log(JSON.stringify(e.json, null, 2));
+      }
+    }
+  }
+
+  // gets table data from the blockchain
+  // and saves it into the component state: "noteTable"
+  getTable() {
+    const rpc = new JsonRpc(endpoint);
+    rpc.get_table_rows({
+      "json": true,
+      "code": "notechain",   // contract who owns the table
+      "scope": "notechain",  // scope of the table
+      "table": "notestruct",    // name of the table as specified by the contract abi
+      "limit": 100,
+    }).then(result => this.setState({ noteTable: result.rows }));
+  }
+
+  componentDidMount() {
+    this.getTable();
+  }
+
+  render() {
+    const { noteTable } = this.state;
+    const { classes } = this.props;
+
+    // generate each note as a card
+    const generateCard = (key, timestamp, user, note) => (
+      <Card className={classes.card} key={key}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            {user}
+          </Typography>
+          <Typography style={{fontSize:12}} color="textSecondary" gutterBottom>
+            {new Date(timestamp+"+00:00").toString()}
+          </Typography>
+          <Typography component="pre">
+            {note}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+    let noteCards = noteTable.map((row, i) =>
+      generateCard(i, row.timestamp, row.user, row.note));
+
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Note Chain
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {noteCards}
+        <Paper className={classes.paper}>
+          <form onSubmit={this.handleFormEvent}>
+            <TextField
+              name="account"
+              autoComplete="off"
+              label="Account"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              name="privateKey"
+              autoComplete="off"
+              label="Private key"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              name="note"
+              autoComplete="off"
+              label="Note (Optional)"
+              margin="normal"
+              multiline
+              rows="10"
+              fullWidth
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.formButton}
+              type="submit">
+              Add / Update note
+            </Button>
+          </form>
+        </Paper>
+        <pre className={classes.pre}>
+          Below is a list of pre-created accounts information for add/update note:
+          <br/><br/>
+          accounts = { JSON.stringify(accounts, null, 2) }
+        </pre>
+      </div>
+    );
+  }
+
+}
+
+export default withStyles(styles)(Index);
